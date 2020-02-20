706046476e55a9c13853841f220b2d0f0c78928f
EC2 Default User <ec2-user@ip-172-31-34-235.us-east-2.compute.internal>
2020-02-20 19:45:50 +0000

    added temp high and low to the site

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (lat, long, callback)=>{
   const url = 'https://api.darksky.net/forecast/9997dd9cfc317d3e00d03b502a145800/'+lat+','+long
-
+//console.log("lat "+lat,"long "+long)
     request({url, json: true}, (error, {body}) =>{
     
     if(error){
@@ -12,7 +12,7 @@ const forecast = (lat, long, callback)=>{
        callback("unable to find location", undefined)
     }
     else{
-        callback(undefined, body.daily.data[0].summary +" It is currently "+ body.currently.temperature +" degress out. There is a " +body.currently.precipProbability +"% Chance of rain")       
+        callback(undefined, body.daily.data[0].summary +" It is currently "+ body.currently.temperature +" degress out. There is a " +body.currently.precipProbability +"% Chance of rain. Expect a high of "+body.daily.data[0].temperatureHigh+"  , and a low of "+body.daily.data[0].temperatureLow+".")       
    }
     
     
